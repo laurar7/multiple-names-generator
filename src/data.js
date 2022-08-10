1e633e62f4ac2b9878b69d8b8efb215572bd1769
Laura R <102334514+laurar7@users.noreply.github.com>
2022-08-10 10:55:38 -0400

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const names = [
+  { person: "Laura", id: "001" },
+  { person: "Luis", id: "002" },
+  { person: "Daniel", id: "003" },
+  { person: "Octavio", id: "004" },
+  { person: "Mery", id: "005" },
+  { person: "Leonel", id: "006" }
+];
